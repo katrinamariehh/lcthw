7f3edd0619c215b4dfc54ef374f1feeff0963a11
katrina <katrina.m.hall@gmail.com>
2014-04-28 18:00:05 -0700

    Committing exercises from today

--- ex3.c
+++ ex3.c
@@ -3,12 +3,12 @@
 
 int main()
 {
-	int age;
+	int age = 35;
 	int height = 0;
 
 	printf("I am %d years old.\n", age);
 	printf("I am %d inches tall.\n", height + 1*15);
-	fprintf(stdout, "pi = %.5f\n", 4 * atan(1.0));
+	printf("pi = %.5f\n", 4 * atan(1.0));
 
 
 	return 0;
